a6657f2dc99404967b58cea4f786ca4561c0d72a
bjekuppen <bjekuppen@gmail.com>
2023-12-20 20:37:29 +0100

    test config

--- test/tests/test_BaseConfig.py
+++ test/tests/test_BaseConfig.py
@@ -1,11 +1,35 @@
 from typed_config import BaseConfig
 
 {
-
-
-
-
-
-
-    
+    "key1": 1,
+    "key2": [
+        1,
+        3
+    ],
+    "key3": [
+        {
+            "key4": 4,
+            "key5": 5
+            }
+    ],
+    "key6": {
+        "key7": 6,
+        "key8": {
+            "key9": {
+                "key10":7,
+                "key11":8
+            }
+        }
+    },
+    "key12":
+        [
+            {
+                "key13": 9,
+                "key14": {
+                    "key15": 10,
+                    "key16": [11, 12, 13]
+         
+                }
+            }
+         ],
 }
